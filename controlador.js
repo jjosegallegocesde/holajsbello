c7937ae098b6caf6db8a9b43e06aeda604239ac1
Juan <juan.gallegomesa@gmail.com>
2021-05-28 15:02:59 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,59 @@
+//CODIGO INICIAL EN JS
+
+//DEFINICION DE VARIABLES
+
+//numeros(enteros/decimales)
+//letras o cadenas de letras (alfanumericos)
+//verdad o booleanos
+
+/*let estudiante="PEDRO PEREZ";
+let edad=20;
+let estatura=1.65;
+let estado=true;*/
+
+/////////////////////////////////
+
+//PROGRAMA RAPPI
+
+//ENTRADAS
+
+//datos del cliente
+/*let nombreCliente="PEDRO PARAMO";
+let identificacionCliente="1017158198";
+let direccionCliente="CR 50 #80-20";
+
+//datos del restaurante
+let nombreRestaurente="Tiendas DELI";
+let direccionRestaurante="cll 100 sur 20-30";
+
+//datos producto
+let nombreProducto="Torta chocolate";
+let precioProducto=25000;
+let cantidadProducto=2;
+
+//PROCESO (calcular el costo total de la compra)
+let costoTotal=(precioProducto*cantidadProducto);
+
+//SALIDA(mostrar en consola el resultado calculado en el proceso)
+console.log("señor: "+nombreCliente+" su costo total es de: "+costoTotal);
+console.log(`señor: ${nombreCliente} su costo total es de: ${costoTotal}`);*/
+
+let nom="PEDRO PARAMO";
+let ident="1017158198";
+let dir="CR 50 #80-20";
+
+//datos del restaurante
+let nomres="Tiendas DELI";
+let dires="cll 100 sur 20-30";
+
+//datos producto
+let nompro="Torta chocolate";
+let x=25000;
+let ctd=2;
+
+//PROCESO (calcular el costo total de la compra)
+let ttl=(x*ctd);
+
+//SALIDA(mostrar en consola el resultado calculado en el proceso)
+console.log("señor: "+nom+" su costo total es de: "+ttl);
+console.log(`señor: ${nom} su costo total es de: ${ttl}`);
